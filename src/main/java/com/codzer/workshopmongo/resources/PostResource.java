3a3bd15d7ec242d6e74a33a56e0073ecdf94b9b3
PabloCodzer <pablo.cozer@gmail.com>
2024-07-07 15:08:09 -0300

    Obtendo post por id

--- src/main/java/com/codzer/workshopmongo/resources/PostResource.java
+++ src/main/java/com/codzer/workshopmongo/resources/PostResource.java
@@ -1,11 +1,14 @@
 package com.codzer.workshopmongo.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codzer.workshopmongo.domain.Post;
@@ -23,5 +26,16 @@ public class PostResource {
 	{
 		List<Post> listPosts = postRepository.findAll();
 		return ResponseEntity.ok().body(listPosts);
+	}
+	
+	@RequestMapping(value = "{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id)
+	{
+		Optional<Post> pst =  postRepository.findById(id);
+		if( !pst.isEmpty())
+		{
+			return ResponseEntity.ok().body(pst.get());
+		}
+		return ResponseEntity.ok().body(null);
 	}
 }
